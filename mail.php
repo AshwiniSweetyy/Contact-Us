d428c88fce1fdf763d38113f8479176166206fe1
Ash <105192418+AshwiniSweetyy@users.noreply.github.com>
2023-03-28 22:02:22 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,33 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$phone= $_POST['phone'];
-$company= $_POST['ompany'];
-$to = "natikarashwini@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Create email headers
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+  // Email subject
+  $subject = "New message from $name";
+
+  // Email body
+  $body = "
+    <p><strong>Name:</strong> $name</p>
+    <p><strong>Email:</strong> $email</p>
+    <p><strong>Message:</strong></p>
+    <p>$message</p>
+  ";
+
+  // Send email
+  $to = 'your-email@example.com'; // Change this to your email address
+  if (mail($to, $subject, $body, $headers)) {
+    echo 'Message sent!';
+  } else {
+    echo 'Message not sent. Please try again.';
+  }
 }
-//redirect
-header("Location:thankyou.html");
 ?>
